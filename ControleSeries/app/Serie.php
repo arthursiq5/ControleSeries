fcdbf05390794219ef5aa8b789d469053afe9ee3
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-23 16:08:41 -0300

    criado objeto para fazer o processamento dos dados para insercao no arquivo SQLite

---
+++ ControleSeries/app/Serie.php
@@ -0,0 +1,8 @@
+<?php
+  namespace App;
+
+  use Illuminate\Database\Eloquent\Model; // ferramenta de conversao de objetos em querys relacionais, e vice-versa
+  class Serie extends Model{
+    // protected $table = 'series'; // removido por ser referencia ao mesmo item
+  }
+ ?>
--- ControleSeries/resources/views/series/create.blade.php
+++ ControleSeries/resources/views/series/create.blade.php
@@ -4,12 +4,12 @@ Adicionar Serie
 @endsection
 
 @section('conteudo')
-<form class="" method="post">
+<form method="post">
     <div class="form-group">
         <label for="nome">Nome</label>
         <input type="text" name="nome" id='nome' class='form-control'/>
     </div>
 
-    <button type="button" class='btn btn-primary'>Adicionar</button>
+    <button class='btn btn-primary'>Adicionar</button>
 </form>
 @endsection
