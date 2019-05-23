6648840bc44c4687e8fc56d41bc502eaed504229
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-23 15:18:46 -0300

    refatorado o PHP do index pra aproveitar melhor os recursos do laravel

--- ControleSeries/resources/views/series/index.blade.php
+++ ControleSeries/resources/views/series/index.blade.php
@@ -8,10 +8,8 @@ Series
     <a href="/series/criar" class='btn btn-dark mb-2'>Adicionar</a>
 
     <ul class='list-group'>
-    <?php
-      foreach ($series as $serie): // forma diferenciada de fazer um foreach com HTML e PHP bem separados
-    ?>
+    @foreach ($series as $serie)
     <li class='list-group-item'><?= $serie?></li>
-  <?php endforeach; ?>
+    @endforeach
 </ul>
 @endsection
