a7e76425fadc14fe702f62b85c2465f59863e3f0
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-28 17:25:40 -0300

    atualizado o banco para comportar campo boolean 'assistido' na tabela 'episodio'; atualizada view para conter um checkbox 'assistido'

--- ControleSeries/resources/views/episodios/index.blade.php
+++ ControleSeries/resources/views/episodios/index.blade.php
@@ -4,11 +4,15 @@
 @endsection
 
 @section('conteudo')
-  <ul class='list-group'>
-    @foreach ($episodios as $episodio)
-      <li class='list-group-item d-flex justify-content-between align-items-center'>
-        Episodio {{ $episodio->numero }}
-      </li>
-    @endforeach
-  </ul>
+  <form class="" action="#" method="post">
+    <ul class='list-group'>
+      @foreach ($episodios as $episodio)
+        <li class='list-group-item d-flex justify-content-between align-items-center'>
+          Episodio {{ $episodio->numero }}
+          <input type="checkbox">
+        </li>
+      @endforeach
+    </ul>
+    <button class='btn btn-primary mt-2 mb-2'>Salvar</button>
+  </form>
 @endsection
